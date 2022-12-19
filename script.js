124d39b411f6e15b5905697d9db8cf2bdda8965a
aminnicevic04 <aminnicevic@gmail.com>
2022-12-19 19:15:04 +0100

    calculator

---
+++ script.js
@@ -0,0 +1,28 @@
+function inflationCalculator(){
+    let inflationRate = document.querySelector('#inflationRate') ;  
+    let money = document.querySelector('#money') ;
+
+    inflationRate = parseFloat(inflationRate.value); 
+    money = parseFloat(money.value) ; 
+
+    let years = document.querySelector('#years').value ; 
+    years = parseFloat(years) ;
+
+    // Formula for calculating inflation.
+    let worth = money + (money * (inflationRate / 100)) ;
+
+    console.log(worth) ;
+
+    for (let i = 1; i < years; i++){
+        worth += worth* (inflationRate / 100) ;
+    } 
+
+    console.log(worth);
+    worth = worth.toFixed(2) ;
+
+    let newElement = document.createElement('div') ;
+    newElement.className = 'new-value' ;
+    newElement.innerText = `Današnjih ${money}€ vredi isto kao ${worth}€ za ${years} godina.` ;
+    document.querySelector('.container').appendChild(newElement) ; 
+
+}
